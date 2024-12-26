2c4e80e0ac426c0cc675bc76750041b187841a87
Ramandeep <deepraman31104@gmail.com>
2024-12-26 15:53:12 +0530

    26-dec-24-2ndTym

--- Justprac/Marauti.java
+++ Justprac/Marauti.java
@@ -1,38 +1,22 @@
 import java.util.*;
 
 public class Marauti{
-
-    public static  boolean isIsomorphic(String s, String t){
-        if(s.length() != t.length()){
-            return false;
-        }
-        HashMap<Character, Character> mp = new HashMap<>();
-        for(int i = 0; i< s.length(); i++){
-            char sch = s.charAt(i);
-            char tch = t.charAt(i);
-            if(mp.containsKey(sch)){
-                if(mp.get(sch) != tch){
-                    return false;
-                }
-                else {
-                    if(mp.containsValue(tch)){
-                    return false;
-                    }
-                    mp.put(sch,tch);
-                }
-            }
-        }
-        return true;
+    public static void solve(String s,int arr[]){
+        arr[1]=1;
+        arr[2]=1;
     }
-    public static void main(String[] args) {
-        String s = "a b c d a";
-        String t = "v w x y z";
+   public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
 
-        if(isIsomorphic(s,t)){
-            System.out.println("yes");
-        }
-        else{
-            System.out.println("not");
-        }
+    System.out.print("Enter your string: ");
+    String s=sc.nextLine();
+
+    int arr[]={1,2,3};
+
+    solve(s,arr);
+    // System.out.println(s);
+    for(int i:arr){
+        System.out.print(i+" ");
     }
+   }
 }
