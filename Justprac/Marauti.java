8017bae5c232ac2d71b2114fbb029e8f2aec8c15
Ramandeep <deepraman31104@gmail.com>
2024-12-27 20:01:17 +0530

    27-12-24

--- Justprac/Marauti.java
+++ Justprac/Marauti.java
@@ -1,22 +1,39 @@
 import java.util.*;
 
 public class Marauti{
-    public static void solve(String s,int arr[]){
-        arr[1]=1;
-        arr[2]=1;
+    public static int solve(int n,int res){
+        if(n<=0){
+            return res;
+        }
+        int dig=n%10;
+        if(dig%2==0)
+        {
+            res=res*10+dig;   
+        }
+        n=n/10;
+        int ans= solve(n,res);
+        return ans;
     }
-   public static void main(String[] args) {
-    Scanner sc=new Scanner(System.in);
-
-    System.out.print("Enter your string: ");
-    String s=sc.nextLine();
-
-    int arr[]={1,2,3};
-
-    solve(s,arr);
-    // System.out.println(s);
-    for(int i:arr){
-        System.out.print(i+" ");
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+       System.out.print(solve(n,0));
+        return;
     }
-   }
 }
+
+
+// static int getPairsCount(int arr[], int n, int k) {
+//     HashMap<Integer, Integer> mp = new HashMap<>();
+//     int count = 0;
+//     for (int i = 0; i < n; i++) {
+//         int d = k - arr[i];
+//         if (mp.containsKey(d)) {
+//             count = count+mp.get(d);
+//         }
+//         int c= mp.getOrDefault(arr[i], 0);
+//         mp.put(arr[i], c+ 1);
+//     }
+//     return count;
+// }
