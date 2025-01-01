25b5e8bde06ace7606674110a3ad9b43a6393719
Ramandeep <deepraman31104@gmail.com>
2025-01-01 12:55:52 +0530

    FindEventualSafeStates-LeetcodeP2

--- Recursion/GenerateParan.java
+++ Recursion/GenerateParan.java
@@ -2,102 +2,68 @@ import java.util.*;
 
 public class GenerateParan {
 
-    public static void solve(int n, int st, int en, StringBuilder s, List<String> ans) {
-        if ((n * 2) / 2 - 1 == en) {
+    public static boolean valid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        if (s.charAt(0) == ')')
+            return false;
+        if (s.charAt(s.length() - 1) == '(')
+            return false;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                st.add(s.charAt(i));
+            } else {
+                if (!st.empty()) {
+                    char c = st.peek();
+                    st.pop();
+                    if (c == s.charAt(i))
+                        return false;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        if (!st.empty())
+            return false;
+        return true;
+    }
+
+    public static void solve(int n, StringBuilder s, List<String> ans,int dp1[]) {
+        if (s.length() == n * 2) {
+            if(valid(s.toString())==true){
+                // System.out.println(s);
+                ans.add(s.toString());
+            }
             return;
         }
-        System.out.println(st + " " + en);
-        StringBuilder res = new StringBuilder(s);
-        int index1 = st;
-        int index2 = en;
 
-        char char1 = res.charAt(index1);
-        char char2 = res.charAt(index2);
+        System.out.println(1);
+        
+        if(dp1[0]>n*2) return;
+        if(dp1[1]>n*2) return;
 
-        res.setCharAt(index1, char2);
-        res.setCharAt(index2, char1);
-
-        // System.out.print(res+" ");
-        ans.add(res.toString());
-        // for(int i=st; i<=n/2+1; i++){
-
-        solve(n, st + 1, en - 1, s, ans);
-        // }
+        s.append("(");
+        dp1[0]=dp1[0]+1;
+        solve(n, s, ans,dp1);
+        dp1[0]=dp1[0]-1;
+        s.deleteCharAt(s.length() - 1);
+        
+        s.append(")");
+        dp1[1]=dp1[1]+1;
+        solve(n, s, ans,dp1);
+        dp1[1]=dp1[1]-1;
+        
+        s.deleteCharAt(s.length() - 1);
     }
-
-    public static List<String> generateParenthesis(int n) {
-        List<String> ans = new ArrayList<>();
-        StringBuilder s = new StringBuilder("");
-
-        for (int i = 0; i < n * 2; i++) {
-            if (i < n * 2 / 2) {
-                s.append("(");
-            } else {
-                s.append(")");
-            }
-        }
-
-        for (int i = 1; i <= n / 2 + 1; i++) {
-            for (int j = s.length()-2; j >0; j--) {
-                StringBuilder res = new StringBuilder(s);
-
-                int index1 = i;
-                int index2 = j;
-        
-                char char1 = s.charAt(index1);
-                char char2 = s.charAt(index2);
-        
-                res.setCharAt(index1, char2);
-                res.setCharAt(index2, char1);
-        
-                System.out.print(res+" ");
-            }
-        }
-
-        // System.out.println(s+" ");
-
-        // System.out.print(s.charAt(1));
-        // System.out.print(s.charAt(n*2-1));
-        // System.out.println(s.length());
-
-        // solve(n, 1, s.length() - 2, s, ans);
-        return ans;
-    }
-
+    
     public static void main(String[] args) {
-        // generateParenthesis(3);
-        int n=8;
+        int dp1[]=new int[2];
+
         List<String> ans = new ArrayList<>();
         StringBuilder s = new StringBuilder("");
-        Set<String> st=new HashSet<>();
-
-        for (int i = 0; i < n * 2; i++) {
-            if (i < n * 2 / 2) {
-                s.append("(");
-            } else {
-                s.append(")");
-            }
-        }
-
-        for (int i = 1; i <= n / 2 + 1; i++) {
-            for (int j = s.length()-2; j >0; j--) {
-                StringBuilder res = new StringBuilder(s);
-
-                int index1 = i;
-                int index2 = j;
         
-                char char1 = s.charAt(index1);
-                char char2 = s.charAt(index2);
-        
-                res.setCharAt(index1, char2);
-                res.setCharAt(index2, char1);
-                
-                st.add(res.toString());
-            }
-        }
-
-        for(String ss:st){
-            System.out.println(ss);
-        }
+        solve(3, s, ans,dp1);
     }
 }
