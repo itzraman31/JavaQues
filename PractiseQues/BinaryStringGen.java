edf94b2abb748e8b15b45c259afddf72f574aede
Ramandeep <deepraman31104@gmail.com>
2025-02-21 00:17:05 +0530

    BinaryStringGen-Leetcode

---
+++ PractiseQues/BinaryStringGen.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class BinaryStringGen {
+    public static void solve(int len,StringBuilder s,ArrayList<String> ans){
+        if(s.length()==len){
+            ans.add(s.toString());
+            return;
+        }
+        s.append("1");
+        solve(len, s, ans);
+        s.deleteCharAt(s.length()-1);
+
+        s.append("0");
+        solve(len, s, ans);
+        s.deleteCharAt(s.length()-1);
+    }
+    public static void main(String[] args) {
+        ArrayList<String> ans=new ArrayList<>();
+        StringBuilder s=new StringBuilder("");
+        solve(16,s,ans);
+
+        System.out.println();
+        for(String str:ans){
+            System.out.print(str+" ");
+        }
+    }    
+}
