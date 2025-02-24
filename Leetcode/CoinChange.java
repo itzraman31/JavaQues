f4bcf782cc90a3b6fa7b10428ed008984cd20b5a
Ramandeep <deepraman31104@gmail.com>
2025-02-24 16:48:06 +0530

    CoinChange-Leetcode

---
+++ Leetcode/CoinChange.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class CoinChange {
+    public static int solve(int[] coins, int amount) {
+        int dp[] = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (i - coins[j] >= 0 && dp[i - coins[j]] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+
+    public static int coinChange(int[] coins, int amount) {
+        return solve(coins, amount);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = sc.nextInt();
+        }
+        int amount = sc.nextInt();
+        System.out.println(coinChange(coins, amount));
+        sc.close();
+    }
+}
