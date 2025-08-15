99293ec96872d287ecc58f9374efa698498a73df
itzraman31 <deepraman31104@gmail.com>
2025-08-15 09:59:41 +0530

    "15-8-25"

---
+++ Trees/Practice.java
@@ -0,0 +1,25 @@
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class Practice {
+
+    public static void levelOrder(Node root){
+        
+    }
+    public static void main(String[] args) {
+
+        Node root = new Node(5);
+
+        System.out.println(root.data);
+
+    }
+}
