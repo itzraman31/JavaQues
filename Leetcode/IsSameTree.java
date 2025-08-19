a28405e80df6f2403e02308cb8c77072115687e7
itzraman31 <deepraman31104@gmail.com>
2025-08-19 17:06:21 +0530

    PreorderTraversal-Leetcode

--- Leetcode/IsSameTree.java
+++ Leetcode/IsSameTree.java
@@ -4,8 +4,14 @@ class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
     TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
@@ -15,22 +21,24 @@ class TreeNode {
 
 public class IsSameTree {
     public static boolean solve(TreeNode p, TreeNode q) {
-        if (p == null && q == null) return true;
-        if (p == null || q == null) return false;
+        if (p == null && q == null)
+            return true;
+        if (p == null || q == null)
+            return false;
         if (p.val == q.val) {
             return solve(p.left, q.left) && solve(p.right, q.right);
         }
         return false;
     }
-    
+
     public static boolean isSameTree(TreeNode p, TreeNode q) {
         return solve(p, q);
     }
-    
+
     public static void main(String[] args) {
         TreeNode tree1 = new TreeNode(1, new TreeNode(2), new TreeNode(3));
         TreeNode tree2 = new TreeNode(1, new TreeNode(2), new TreeNode(3));
-        
+
         System.out.println(isSameTree(tree1, tree2));
     }
 }
---
+++ Leetcode/PreorderTraversal.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class PreorderTraversal {
+    public static List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            ans.add(node.val);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+        System.out.println(preorderTraversal(root));
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
